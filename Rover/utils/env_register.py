913af2eee5cdb3d88805ed7d3b0a3a1d9dda1483
luiz <luiz.marao@gmail.com>
2023-04-11 15:28:41 -0300

    env_register now runs properly

--- Rover/utils/env_register.py
+++ Rover/utils/env_register.py
@@ -1,8 +1,9 @@
 from gymnasium import register
-import Environments.rover_4We_v1 as roverenvs
+from Rover.Environments.rover_4We_v1 import RoverRobotrek4Wev1Env as roverv1
 
 
 
 def register_rover_environments():
-    register(id="Rover4We-v1", entry_point='Environments.rover_4We_v1.roverenvs.RoverRobotrek4Wev1Env')
+    register(id="Rover4We-v1", entry_point='Rover.Environments.rover_4We_v1:RoverRobotrek4Wev1Env')
+    register(id="Rover4We-v2", entry_point='Rover.Environments.rover_4We_v1:RoverRobotrek4Wev2Env')
 
