3a2d4162ce96eb5008cfd6793713a5a9e3e9d65c
luiz <luiz.marao@gmail.com>
2023-04-16 14:25:27 -0300

    update env_register.py

--- Rover/utils/env_register.py
+++ Rover/utils/env_register.py
@@ -1,9 +1,8 @@
 from gymnasium import register
-from Rover.Environments.rover_4We_v1 import RoverRobotrek4Wev1Env as roverv1
 
 
 
 def register_rover_environments():
     register(id="Rover4We-v1", entry_point='Rover.Environments.rover_4We_v1:RoverRobotrek4Wev1Env')
-    register(id="Rover4We-v2", entry_point='Rover.Environments.rover_4We_v1:RoverRobotrek4Wev2Env')
+    # TODO: fix registering for rover2cam #register(id="Rover4We-v2", entry_point='Rover.Environments.rover_4We_')
 
