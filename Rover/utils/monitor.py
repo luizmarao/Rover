ff067b0971c4059ba2e7aa74667a4fdee38c142d
luiz <luiz.marao@gmail.com>
2023-04-16 14:08:50 -0300

    changed RoverMonitor class to properly retrieving info from dict and acquiring reset_info from environment

--- Rover/utils/monitor.py
+++ Rover/utils/monitor.py
@@ -20,7 +20,7 @@ class RoverMonitor(Monitor):
             ep_len = len(self.rewards)
             ep_info = {"r": round(ep_rew, 6), "l": ep_len, "t": round(time.time() - self.t_start, 6)}
             for key in self.info_keywords:
-                ep_info[key] = info[key]
+                ep_info[key] = info.get(key)
             self.episode_returns.append(ep_rew)
             self.episode_lengths.append(ep_len)
             self.episode_times.append(time.time() - self.t_start)
@@ -30,3 +30,22 @@ class RoverMonitor(Monitor):
             info["episode"] = ep_info
         self.total_steps += 1
         return observation, reward, terminated, truncated, info
+
+    def reset(self, **kwargs) -> Tuple[ObsType, Dict[str, Any]]:
+        """
+        Calls the Gym environment reset. Can only be called if the environment is over, or if allow_early_resets is True
+
+        :param kwargs: Extra keywords saved for the next episode. only if defined by reset_keywords
+        :return: the first observation of the environment
+        """
+        if not self.allow_early_resets and not self.needs_reset:
+            raise RuntimeError(
+                "Tried to reset an environment before done. If you want to allow early resets, "
+                "wrap your env with Monitor(env, path, allow_early_resets=True)"
+            )
+        self.rewards = []
+        self.needs_reset = False
+        obs, info = self.env.reset(**kwargs)
+        for key in self.reset_keywords:
+            self.current_reset_info[key] = info[key]
+        return obs, info
