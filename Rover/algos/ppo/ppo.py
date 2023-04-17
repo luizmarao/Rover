196ee46151ee071c5d36ffa5629c1443a9dd3274
luiz <luiz.marao@gmail.com>
2023-04-17 19:51:21 -0300

    include network ranking import in PPO_Rover

--- Rover/algos/ppo/ppo.py
+++ Rover/algos/ppo/ppo.py
@@ -14,9 +14,11 @@ from gymnasium import spaces
 from torch.nn import functional as F
 
 from stable_baselines3.common.on_policy_algorithm import OnPolicyAlgorithm
-from stable_baselines3.common.policies import ActorCriticCnnPolicy, ActorCriticPolicy, BasePolicy, MultiInputActorCriticPolicy
+from stable_baselines3.common.policies import ActorCriticCnnPolicy, ActorCriticPolicy, BasePolicy, \
+    MultiInputActorCriticPolicy
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
 from stable_baselines3.common.utils import explained_variance, get_schedule_fn, obs_as_tensor, safe_mean
+from Rover.utils.networks_ranking import RoverRankingSystem
 
 SelfPPO = TypeVar("SelfPPO", bound="PPO")
 class PPO_Rover(PPO):
