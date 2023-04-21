8c960d2a27ace6101722df879af978583f5cadbf
luiz <luiz.marao@gmail.com>
2023-04-21 19:19:54 -0300

    rename file for Rover4WeEncodedVision envs, and add env to register function

--- Rover/utils/env_register.py
+++ Rover/utils/env_register.py
@@ -4,5 +4,7 @@ from gymnasium import register
 
 def register_rover_environments():
     register(id="Rover4We-v2", entry_point='Rover.Environments.rover_4We_v2:RoverRobotrek4Wev2Env')
+    register(id="Rover4WeEncodedVision-v0", entry_point='Rover.Environments.rover_4We_encoded_vision'
+                                                        ':Rover4WeEncodedVisionv0Env')
     # TODO: fix registering for rover2cam #register(id="Rover4We-v2", entry_point='Rover.Environments.rover_4We_')
 
