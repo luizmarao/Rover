fd1e06562a9c1fe6f01ffba0316ad9d4668f8d0a
luiz <luiz.marao@gmail.com>
2023-04-21 20:13:03 -0300

    fix rover_4We_encoded_vision.py __init__ to properly work with encoded dimensions

--- Rover/Environments/rover_4We_encoded_vision.py
+++ Rover/Environments/rover_4We_encoded_vision.py
@@ -5,12 +5,14 @@ import numpy as np
 
 class Rover4WeEncodedVisionv0Env(RoverRobotrek4Wev2Env):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.encoder_name = kwargs.get('encoder_name')
+        self.encoder_name = kwargs.pop('encoder_name')
         assert self.encoder_name is not None, 'You have not chose any encoder. If you do not want to chose one, you should run Rover4We latest version instead of Rover4WeEncodedVision'
         encoder_path = os.path.join(os.path.dirname(__file__), 'assets', 'VisionEncoders', self.encoder_name)  # TODO: check img_red_size compatible with encoder input size
         self.encoder = keras.models.load_model(encoder_path)
         self.encoder.compile()
+        self.observation_space_size = np.product(self.encoder.output_shape[1:]) + 14
+        super().__init__(**kwargs)
+
 
     def camera_rendering(self):
         gray_normalized = super().camera_rendering()
