e2740d9a320c71073ff70d6d67852cf6fe9effc1
luiz <luiz.marao@gmail.com>
2023-04-21 21:27:24 -0300

    added rover 2 cam (Packed and Fused versions) to register

--- Rover/utils/env_register.py
+++ Rover/utils/env_register.py
@@ -6,5 +6,8 @@ def register_rover_environments():
     register(id="Rover4We-v2", entry_point='Rover.Environments.rover_4We_v2:RoverRobotrek4Wev2Env')
     register(id="Rover4WeEncodedVision-v0", entry_point='Rover.Environments.rover_4We_encoded_vision'
                                                         ':Rover4WeEncodedVisionv0Env')
-    # TODO: fix registering for rover2cam #register(id="Rover4We-v2", entry_point='Rover.Environments.rover_4We_')
+    register(id="Rover4WeDoubleCameraPacked-v0", entry_point='Rover.Environments.rover_4We_DoubleCam'
+                                                        ':Rover4WeDoubleCameraPackedv0Env')
+    register(id="Rover4WeDoubleCameraFused-v0", entry_point='Rover.Environments.rover_4We_DoubleCam'
+                                                             ':Rover4WeDoubleCameraFusedv0Env')
 
