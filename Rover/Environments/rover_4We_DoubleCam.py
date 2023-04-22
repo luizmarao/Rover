77eae248a97bd3778b5117835d849e20b4d9d91f
luiz <luiz.marao@gmail.com>
2023-04-21 21:00:19 -0300

    initial converted version of Rover with 2 cameras (need testing)

---
+++ Rover/Environments/rover_4We_DoubleCam.py
@@ -0,0 +1,143 @@
+'''
+Arquivo de configuração do ambient rover do mujoco
+
+Endereço do arquivo : gym/gym/envs/mujoco
+
+Baseado em: https://github.com/openai/gym/tree/master/gym/envs/mujoco
+
+rover-4-wheels-diff-ackerman-double-front-wheel.xml qpos and qvel
+
+body names:     ['rover', 'r-l-wheel', 'r-r-wheel', 'ghost-steer-wheel', 'f-l-wheel', 'f-l-axis', 'f-l-l-wheel',
+                'f-l-r-wheel', 'f-r-wheel', 'f-r-axis', 'f-r-l-wheel', 'f-r-r-wheel']
+body id:        [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12]
+body_geomadr:   [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+
+qpos[0] = rover's x position
+qpos[1] = rover's y position
+qpos[2] = rover's z position
+qpos[3] = rover's w quaternion vector
+qpos[4] = rover's a quaternion vector
+qpos[5] = rover's b quaternion vector
+qpos[6] = rover's c quaternion vector
+qpos[7] = rear left wheel rotation angle
+qpos[8] = rear right wheel rotation angle
+qpos[9] = ghost steer hinge
+qpos[10]= front left wheel steer angle
+qpos[11]= front left wheel rotation 1 angle
+qpos[12]= front left wheel rotation 2 angle
+qpos[13]= front right wheel steer angle
+qpos[14]= front right wheel rotation 3 angle
+qpos[15]= front right wheel rotation 4 angle
+
+qvel[0] = rover's x velocity
+qvel[1] = rover's y velocity
+qvel[2] = rover's z velocity
+qvel[3] = rover's x angular velocity
+qvel[4] = rover's y angular velocity
+qvel[5] = rover's z angular velocity
+qvel[6] = rear left wheel angular velocity
+qvel[7] = rear right wheel angular velocity
+qvel[8] = ghost steer hinge velocity
+qvel[9]= front left wheel steer velocity
+qvel[10]= front left wheel rotation 1 velocity
+qvel[11]= front left wheel rotation 2 velocity
+qvel[12]= front right wheel steer velocity
+qvel[13]= front right wheel rotation 3 velocity
+qvel[14]= front right wheel rotation 4 velocity
+
+
+
+rover-4-wheels.xml qpos and qvel
+
+qpos[0] = rover's x position
+qpos[1] = rover's y position
+qpos[2] = rover's z position
+qpos[3] = rover's w quaternion vector
+qpos[4] = rover's a quaternion vector
+qpos[5] = rover's b quaternion vector
+qpos[6] = rover's c quaternion vector
+qpos[7] = rear left wheel rotation angle
+qpos[8] = rear right wheel rotation angle
+qpos[9] = steerbar rotation angle
+qpos[10]= front left wheel rotation angle
+qpos[11]= front right wheel rotation angle
+qpos[12]= drive motor rotation angle (the prism between rear wheels)
+
+qvel[0] = rover's x velocity
+qvel[1] = rover's y velocity
+qvel[2] = rover's z velocity
+qvel[3] = rover's x angular velocity
+qvel[4] = rover's y angular velocity
+qvel[5] = rover's z angular velocity
+qvel[6] = rear left wheel angular velocity
+qvel[7] = rear right wheel angular velocity
+qvel[8] = steerbar angular velocity
+qvel[9]= front left wheel angular velocity
+qvel[10]= front right wheel angular velocity
+qvel[11]= drive motor angular velocity (the prism between rear wheels)
+
+
+rover-4-wheels_loose.xml qpos and qvel
+
+qpos[0] = rover's x position
+qpos[1] = rover's y position
+qpos[2] = rover's z position
+qpos[3] = rover's w quaternion vector
+qpos[4] = rover's a quaternion vector
+qpos[5] = rover's b quaternion vector
+qpos[6] = rover's c quaternion vector
+qpos[7] = rear left wheel steer rotation angle		(very limited)
+qpos[8] = rear left wheel rolling rotation angle
+qpos[9] = rear right wheel steer rotation angle		(very limited)
+qpos[10]= rear right wheel rolling rotation angle
+qpos[11]= front left wheel steer rotation angle
+qpos[12]= front left wheel rolling rotation angle
+qpos[13]= front right wheel steer rotation angle
+qpos[14]= front right wheel rolling rotation angle
+
+qvel[0] = rover's x velocity
+qvel[1] = rover's y velocity
+qvel[2] = rover's z velocity
+qvel[3] = rover's x angular velocity
+qvel[4] = rover's y angular velocity
+qvel[5] = rover's z angular velocity
+qvel[6] = rear left wheel steer angular velocity
+qvel[7] = rear left wheel rolling angular velocity
+qvel[8] = rear right wheel steer angular velocity
+qvel[9]= rear right wheel rolling angular velocity
+qvel[10]= front left wheel steer angular velocity
+qvel[11]= front left wheel rolling angular velocity
+qvel[12]= front right wheel steer angular velocity
+qvel[13]= front right wheel rolling angular velocity
+
+'''
+
+import numpy as np
+import cv2
+from gymnasium import utils
+from gymnasium.envs.mujoco import MujocoEnv
+from gymnasium import spaces
+#import matplotlib.pyplot as plt
+import mujoco
+import os
+from Rover.Environments.rover_4We_v2 import RoverRobotrek4Wev2Env
+
+# O campo tem dimensoes (x,y)=(44,25) [metros]
+# É uma matriz (x,y) de (0,0) até (3, 4) elementos, em que cada elemento tem dimensão (x,y) de (44/4, 25/5) = (11.0 , 5.0) [metros]
+# Na configuração A, os elementos (2, 1) e (2, 3) tem obstáculos
+# Na configuração B, os elementos (1, 2) e (3, 2) tem obstáculos
+
+
+class Rover4WeDoubleCameraPackedv0Env(RoverRobotrek4Wev2Env):
+    def __init__(self, **kwargs):
+        self.model_file_name = 'main-trekking-challenge-4wheels_diff-acker-double-front-wheel-2cam.xml'
+        self.observation_space_size = 2*np.product(self.img_reduced_size) + 14
+        self.camera_r = 'first-person-r'
+        self.camera_l = 'first-person-l'
+        super().__init__(**kwargs)
+    def camera_rendering(self):
+        gray_normalized_img_r = super().camera_rendering(camera_name='first-person-r',extra_img_name='r_')
+        gray_normalized_img_l = super().camera_rendering(camera_name='first-person-l', extra_img_name='l_')
+        packed_normalized_gray = np.dstack((gray_normalized_img_l, gray_normalized_img_r))
+        return packed_normalized_gray / 255.0
+
