173891e27fad2006ba15295c59b339caafc84695
luiz <luiz.marao@gmail.com>
2023-05-03 12:24:30 -0300

    update env register to include Rover4W-v1

--- Rover/utils/env_register.py
+++ Rover/utils/env_register.py
@@ -10,4 +10,4 @@ def register_rover_environments():
                                                         ':Rover4WeDoubleCameraPackedv0Env')
     register(id="Rover4WeDoubleCameraFused-v0", entry_point='Rover.Environments.rover_4We_DoubleCam'
                                                              ':Rover4WeDoubleCameraFusedv0Env')
-
+    register(id="Rover4W-v1", entry_point='Rover.Environments.rover_4W:Rover4Wv1Env')
