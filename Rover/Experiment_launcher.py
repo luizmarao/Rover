e671f6449e5804c550fbd27c496993d4ee535977
luiz <luiz.marao@gmail.com>
2023-05-10 21:41:51 -0300

    updates in Experiment_launcher.py

--- Rover/Experiment_launcher.py
+++ Rover/Experiment_launcher.py
@@ -1,21 +1,20 @@
-import torch as th
-import torch.nn as nn
-from gymnasium import spaces
-import numpy as np
 import os
 
-from Rover.utils.env_util import make_vec_env
-from Rover.utils.networks import RovernetClassic
-from Rover.utils.logger import configure
-from Rover.utils.env_register import register_rover_environments
+import numpy as np
+
 from Rover.algos.ppo.ppo import PPO_Rover
+from Rover.utils.env_register import register_rover_environments
+from Rover.utils.env_util import make_vec_env
+from Rover.utils.logger import configure
+from Rover.utils.lr_schedulers import linear_schedule
+from Rover.utils.networks import RovernetClassic
 from Rover.utils.networks_ranking import RoverRankingSystem
 
 register_rover_environments()
 
 if __name__ == "__main__":
     os.environ["MUJOCO_GL"] = 'egl'  # Set mujoco rendering to dedicated GPU
-    EXPERIMENT_NAME = 'TestRoverSB3'
+    EXPERIMENT_NAME = 'TestRoverPackedSB3'
     os.environ["SB3_LOGDIR"] = os.path.join(os.path.dirname(__file__), 'Experiments')
     assert isinstance(os.environ["SB3_LOGDIR"], str)
     os.makedirs(os.environ["SB3_LOGDIR"], exist_ok=True)
@@ -25,15 +24,16 @@ if __name__ == "__main__":
 
     ## ENVIRONMENT PARAMETERS ##
     rover_env = "Rover4We-v2"
-    img_red_size = (32, 32)
+    img_red_size = (64, 64)
     rover_2cam_and_packed_images = False
     dynamic_obs_size = 14
-    num_environments = 4
-    env_kwargs = {"render_mode": "rgb_array", "width": 440, "height": 270, "save_images": False}
+    num_environments = 8
+    save_images = False
     start_at_initpos = False
     end_after_current_goal = True
     random_current_goal = True
     monitor_kwargs = dict(info_keywords=['death', 'goal_reached_flag', 'timeout'], reset_keywords=['current_goal'])
+    env_verbose = 0
 
     ## NETWORK STRUCTURE PARAMETERS ##
     Networks_Architecture = RovernetClassic
@@ -43,27 +43,23 @@ if __name__ == "__main__":
         (64, 2, 1)
     ]
     features_extractor_lin_layers = None  # linear layers after conv layers and before mlp layers [size] ex:[64, 64]
-    share_features_extractor = True
+    share_features_extractor = False
     net_arch = dict(pi=[64, 64], vf=[64, 64])  # mlp extractor after conv layers (feature extractor)
-    if features_extractor_lin_layers is not None:
-        features_dim = features_extractor_lin_layers[-1]
-    else:
-        features_dim = np.product(img_red_size) + dynamic_obs_size
     normalize_images = False  # Already done in env
 
     ## PPO PARAMETERS ##
-    total_learning_timesteps = int(5e5)
+    total_learning_timesteps = int(5e6)
     n_steps = 2048  # for each env per update
     seed = None
-    learning_rate = 0.0001
+    learning_rate = linear_schedule(0.00005)
     gamma = 0.99
-    n_epochs = 10  # networks training epochs
+    n_epochs = 20  # networks training epochs
     gae_lambda = 0.95
-    batch_size = 256  # was 64in default algo
+    batch_size = 256  # was 64 in default algo
     clip_range = 0.1  # was 0.3 in default algo
     normalize_advantage = True
     ent_coef = 0.0
-    max_grad_norm = 1.0  # was 0.5 in default algo
+    max_grad_norm = 0.5  # was 0.5 in default algo
     use_sde = False
     target_kl = 0.1  # was None in default algo
     stats_window_size = 100
@@ -76,7 +72,6 @@ if __name__ == "__main__":
         normalize_images=normalize_images,
         net_arch=net_arch,
         features_extractor_kwargs=dict(
-            features_dim=features_dim,
             img_red_size=img_red_size,
             rover_2cam_and_packed_images=rover_2cam_and_packed_images,
             dynamic_obs_size=dynamic_obs_size,
@@ -85,6 +80,8 @@ if __name__ == "__main__":
         ),
     )
 
+    env_kwargs = {"render_mode": "rgb_array", "width": 440, "height": 270, "save_images": save_images, "verbose": env_verbose,
+                  "img_reduced_size": img_red_size, "gamma": gamma}
     envs = make_vec_env(rover_env, env_kwargs=env_kwargs, n_envs=num_environments, monitor_kwargs=monitor_kwargs)
     model = PPO_Rover("CnnPolicy", envs, policy_kwargs=policy_kwargs, verbose=1,
                       n_steps=n_steps,
@@ -104,7 +101,7 @@ if __name__ == "__main__":
                       clear_ep_info_buffer_every_iteration=clear_ep_info_buffer_every_iteration
                       )
     rover_rankings = RoverRankingSystem(networks_limit_per_ranking=5, num_rankings=3,
-                                       save_path=logger.get_dir(), networks_subfolder='saved_networks', verbose=1)
+                                       save_path=logger.get_dir(), networks_subfolder='saved_networks', verbose=0)
     model.set_ranking_system(rover_rankings)
     model.set_logger(logger)
     model.learn(total_timesteps=total_learning_timesteps, progress_bar=True)
