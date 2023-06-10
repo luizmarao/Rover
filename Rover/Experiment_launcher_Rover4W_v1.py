b1cf48a0217a9d50833b0de5f7be2e0f78c056ba
luiz <luiz.marao@gmail.com>
2023-06-09 23:35:48 -0300

    update Experiment_launcher_Rover4W_v1.py to currently best configuration

--- Rover/Experiment_launcher_Rover4W_v1.py
+++ Rover/Experiment_launcher_Rover4W_v1.py
@@ -13,7 +13,7 @@ register_rover_environments()
 
 if __name__ == "__main__":
     os.environ["MUJOCO_GL"] = 'egl'  # Set mujoco rendering to dedicated GPU
-    EXPERIMENT_NAME = 'TestRover4Wv1'
+    EXPERIMENT_NAME = 'TestRover4Wv1_4env_4096ns_bs8192_60e_newrewsys_seed10'  # ns=num steps, bs=batch size, e=epochs, gr=goal_rwd
     os.environ["SB3_LOGDIR"] = os.path.join(os.path.dirname(__file__), 'Experiments')
     assert isinstance(os.environ["SB3_LOGDIR"], str)
     os.makedirs(os.environ["SB3_LOGDIR"], exist_ok=True)
@@ -24,30 +24,31 @@ if __name__ == "__main__":
     ## ENVIRONMENT PARAMETERS ##
     rover_env = "Rover4W-v1"
     num_environments = 4
-    env_kwargs = {"render_mode": "human"}
+    env_kwargs = {"render_mode": "rgb_array", 'goal_rwd': 5, 'control_cost': 0.0007, 'time_pnlt': 0.00012,
+                  'svv_rew':  0.0}
     start_at_initpos = False
     end_after_current_goal = True
     random_current_goal = True
     monitor_kwargs = dict(info_keywords=['death', 'goal_reached_flag', 'timeout'], reset_keywords=['current_goal'])
 
     ## NETWORK STRUCTURE PARAMETERS ##
-    net_arch = dict(pi=[64, 64], vf=[64, 64])  # mlp extractor after conv layers (feature extractor)
+    net_arch = dict(pi=[64, 64], vf=[64, 64])
 
     ## PPO PARAMETERS ##
-    total_learning_timesteps = int(5e6)
-    n_steps = 2048  # for each env per update
-    seed = None
-    learning_rate = linear_schedule(0.0001)
+    total_learning_timesteps = int(1e7)
+    n_steps = 4096  # for each env per update
+    seed = 20
+    learning_rate = linear_schedule(0.00005)
     gamma = 0.99
-    n_epochs = 15  # networks training epochs
+    n_epochs = 60  # networks training epochs
     gae_lambda = 0.95
-    batch_size = 64  # was 64 in default algo
-    clip_range = 0.1  # was 0.3 in default algo
+    batch_size = 4*2048  # was 64 in default algo
+    clip_range = 0.08  # was 0.3 in default algo
     normalize_advantage = True
     ent_coef = 0.1
-    max_grad_norm = 1.0  # was 0.5 in default algo
+    max_grad_norm = 0.5  # was 0.5 in default algo
     use_sde = False
-    target_kl = 0.1  # was None in default algo
+    target_kl = 0.08  # was None in default algo
     stats_window_size = 100
     clear_ep_info_buffer_every_iteration = True
 
@@ -74,7 +75,7 @@ if __name__ == "__main__":
                       clear_ep_info_buffer_every_iteration=clear_ep_info_buffer_every_iteration
                       )
     rover_rankings = RoverRankingSystem(networks_limit_per_ranking=5, num_rankings=3,
-                                       save_path=logger.get_dir(), networks_subfolder='saved_networks', verbose=1)
+                                       save_path=logger.get_dir(), networks_subfolder='saved_networks', verbose=0)
     model.set_ranking_system(rover_rankings)
     model.set_logger(logger)
     model.learn(total_timesteps=total_learning_timesteps, progress_bar=True)
