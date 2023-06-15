837ad89b1f1110c6c419aaf69c3d8dce9dd00740
luiz <luiz.marao@gmail.com>
2023-06-15 16:52:23 -0300

    create an functional version of the GUI plotting tool. Needs fix to differentiate experiments when adding a new one. Also needs new features

--- Rover/utils/plotting_GUI.py
+++ Rover/utils/plotting_GUI.py
@@ -3,31 +3,156 @@ import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
+from matplotlib import pyplot as plt
 
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 import os
 from os import path as osp
+import csv
+import re
+import numpy as np
+import time
 
+
+
+
+
+BLACK       = '\033[30m'
+RED         = '\033[31m'
+GREEN       = '\033[32m'
+ORANGE	    = '\033[33m'
+BLUE	    = '\033[34m'
+MAGENTA	    = '\033[35m'
+CYAN	    = '\033[36m'
+LIGHTGRAY	= '\033[37m'
+YELLOW	    = '\033[93m'
+DEFAULT	    = '\033[39m'
+ENDCOLOR    = '\033[0m'
+
+class FileWatcher:
+    def __init__(self, file_path):
+        self._cached_stamp = 0
+        self.filename = file_path
+
+    def has_news(self):
+        stamp = os.stat(self.filename).st_mtime
+        if stamp != self._cached_stamp:
+            self._cached_stamp = stamp
+            return True
+        else:
+            return False
+
+# from here: https://stackoverflow.com/a/14314054/6609908
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
 
 class PlotArea(tk.Frame):
 
-    def __init__(self, parent, controller):
+    def __init__(self, parent, controller, progress_files):
+        self.set_progress_files(progress_files)
         tk.Frame.__init__(self, parent)
 
-        f = Figure(figsize=(5, 5), dpi=100)
-        a = f.add_subplot(111)
-        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
+        self.line_plot_list = []
+        self.figure = Figure(figsize=(5, 5))
+        self.ax = self.figure.add_subplot(111)
+        # aux_plot, = self.ax.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
+        # self.line_plot_list.append(aux_plot)
 
-        canvas = FigureCanvasTkAgg(f, self)
+        self.canvas = FigureCanvasTkAgg(self.figure, self)
         # canvas.show()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
-        toolbar = NavigationToolbar2Tk(canvas, self)
-        toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self)
+        self.toolbar.update()
+        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
+    def set_progress_files(self, progress_files):
+        self.progress_files = progress_files
+        self.file_watchers = [FileWatcher(progress_file_path) for progress_file_path in progress_files]
+
+    def add_progress_file(self, progress_file):
+        self.progress_files += [progress_file]
+        self.file_watchers += [FileWatcher(progress_file)]
+
+    def plot_update(self):
+        for progress_file_path in self.progress_files:
+            with open(progress_file_path, 'r') as progress_file:
+                csv_reader = csv.DictReader(progress_file, delimiter=',')
+
+                fields = list(next(csv_reader).keys())
+
+                xlabel = 'time/total_timesteps'
+                smooth_window = 20
+                selected_fields = ["rollout/success_rate (%)", "rollout/ep_rew_mean", "rollout/ep_rew_min", "rollout/ep_rew_max", "rollout/Avg÷MEA (%)"]
+                is_selecting_fields = False
+                solid_line_re = re.compile('-')
+                dashed_line_re = re.compile('--')
+                dotted_line_re = re.compile(r'\*')
+                dash_dotted_line_re = re.compile(r'\*-')
+                line_types = {selected_fields[-1]: '-'}
+                title = ''
+                xlabel_custom = ''
+                ylabel = ''
+                xmax = 0
+
+                yvalues = {f: [] for f in selected_fields}
+                xvalues = []
+                progress_file.seek(0)
+                next(csv_reader)
+                for row in csv_reader:
+                    xvalues.append(float(row[xlabel]))
+                    for sf in selected_fields:
+                        yvalues[sf].append(float(row[sf]))
+
+                if smooth_window > 1:
+                    for sf in selected_fields:
+                        yvalues[sf] = moving_average(yvalues[sf], smooth_window)
+                    xvalues = xvalues[smooth_window - 1:]
+                last_index = 0
+                first_time = True
+
+                for i, sf in enumerate(selected_fields):
+                    if xmax == 0:
+                        if first_time:
+                            aux_plot, = self.ax.plot(xvalues, yvalues[sf], label='name')
+                            self.line_plot_list.append(aux_plot)
+                        else:
+                            self.line_plot_list[i].set_ydata(yvalues[sf])
+                            self.line_plot_list[i].set_xdata(xvalues)
+                            max_y = np.max([max_y, np.max(yvalues[sf])])
+                            min_y = np.min([min_y, np.min(yvalues[sf])])
+                    else:
+                        if first_time:
+                            aux_plot = self.ax.plot(xvalues[:last_index], yvalues[sf][:last_index], label='name')
+                            self.line_plot_list.append(aux_plot)
+                        else:
+                            self.line_plot_list[i].set_ydata(yvalues[sf][:last_index])
+                            self.line_plot_list[i].set_xdata(xvalues[:last_index])
+                            max_y = np.max([max_y, np.max(yvalues[sf][:last_index])])
+                            min_y = np.min([min_y, np.min(yvalues[sf][:last_index])])
+                if first_time:
+                    self.ax.ticklabel_format(style='sci', scilimits=(0, 3))
+                    self.ax.grid(True)
+                    self.ax.set_xlabel(xlabel) if xlabel_custom == '' else self.ax.set_xlabel(xlabel_custom)
+                    if not ylabel == '':
+                        self.ax.set_ylabel(ylabel)
+                    self.ax.set_title(title)
+                    self.figure.legend(selected_fields).set_draggable(True)
+                    # plt.ion()
+                    # self.figure.show()
+                else:
+                    self.ax.set_xlim(0, xvalues[-1] * 1.01 if xmax == 0 else xvalues[:last_index][-1] * 1.01)
+                    self.ax.set_ylim(min_y * 1.01, max_y * 1.01)
+                    #self.ax.draw()
+                    # plt.pause(0.001)
+                    # time.sleep(1.)
+                    #self.figure.canvas.flush_events()
+                first_time = False
+        self.canvas.draw()
 
 class App(tk.Tk):
     num_tree_items = 0
@@ -36,12 +161,13 @@ class App(tk.Tk):
         super().__init__()
         #self.geometry("900x700")
         self.title("Rover Plotting Tool")
+        self.grid()
         self.resizable(True, True)
         self.create_main_window()
 
     def create_exp_selector_side(self):
-        left_frame = ttk.Frame(self)
-        # left_frame.columnconfigure(0, weight=1)
+        left_frame = ttk.Frame(self, height=500, width=200)
+        left_frame.columnconfigure(3, weight=6)
 
         tree_label = ttk.Label(left_frame, text="Folders")
         tree_label.grid(column=0, row=0, sticky=tk.W)
@@ -54,39 +180,48 @@ class App(tk.Tk):
         remove_button = ttk.Button(left_frame, text="-", command=self.remove_folder)
         remove_button.grid(column=2, row=0, sticky=tk.W)
 
-        button3 = ttk.Button(left_frame, text='3')
-        button3.grid(column=3, row=0, sticky=tk.W)
-        button4 = ttk.Button(left_frame, text='4')
-        button4.grid(column=4, row=0, sticky=tk.W)
-        button5 = ttk.Button(left_frame, text='5')
-        button5.grid(column=5, row=0, sticky=tk.W)
-        button6 = ttk.Button(left_frame, text='6')
-        button6.grid(column=6, row=0, sticky=tk.W)
+        # button3 = ttk.Button(left_frame, text='3')
+        # button3.grid(column=3, row=0, sticky=tk.W)
+        # button4 = ttk.Button(left_frame, text='4')
+        # button4.grid(column=4, row=0, sticky=tk.W)
+        # button5 = ttk.Button(left_frame, text='5')
+        # button5.grid(column=5, row=0, sticky=tk.W)
+        # button6 = ttk.Button(left_frame, text='6')
+        # button6.grid(column=6, row=0, sticky=tk.W)
 
-        tree = ttk.Treeview(left_frame, selectmode="browse")
-        tree.grid(column=0, row=1, sticky=tk.NSEW, columnspan=7)
+        tree = ttk.Treeview(left_frame, selectmode="browse", height=40)
+        tree.grid(column=0, row=1, sticky=tk.NSEW, columnspan=4)
 
-        tree_scroll = ttk.Scrollbar(left_frame, orient='vertical', command=tree.yview())
-        tree_scroll.grid(column=7, row=1, sticky='NSW')
+        tree_vscroll = ttk.Scrollbar(left_frame, orient='vertical', command=tree.yview())
+        tree_vscroll.grid(column=4, row=1, sticky='NSW')
 
-        tree['yscrollcommand'] = tree_scroll.set
+        tree['yscrollcommand'] = tree_vscroll.set
+
+        tree_hscroll = ttk.Scrollbar(left_frame, orient='horizontal', command=tree.xview())
+        tree_hscroll.grid(column=0, row=2, sticky='WE', columnspan=4)
+
+        tree['xscrollcommand'] = tree_hscroll.set
         self.tree = tree
+
+        plot_button = ttk.Button(left_frame, text='Plot Selected', command=self.plot_selected)
+        plot_button.grid(column=0, row=3, sticky=tk.W)
+
+        add_plot_button = ttk.Button(left_frame, text='Add to Plot', command=self.add2plot)
+        add_plot_button.grid(column=1, row=3, sticky=tk.W)
 
         return left_frame
 
     def create_plot_side(self):
-        right_frame = ttk.Frame(self)
+        right_frame = ttk.Frame(self, height=800, width=800)
         right_frame.grid(column=2, row=0, sticky=tk.NSEW)
 
-        tab_control = ttk.Notebook(right_frame)
-        tab_control.grid(column=0, row=0)
+        self.tab_control = ttk.Notebook(right_frame, height=800, width=800)
+        self.tab_control.grid(column=0, row=0, sticky=tk.NSEW)
 
-        # tab1 = ttk.Frame(tab_control)
-        tab1 = PlotArea(tab_control, self)
-        tab_control.add(tab1, text="Plot 1")
-
-        tab2 = ttk.Frame(tab_control)
-        tab_control.add(tab2, text="Plot 2")
+        tab1 = PlotArea(self.tab_control, self, progress_files=[])
+        # tab2 = PlotArea(self.tab_control, self, progress_files=[])
+        self.tab_control.add(tab1, text="Plot 1")
+        # self.tab_control.add(tab2, text="Plot 2")
 
         return right_frame
 
@@ -103,13 +238,9 @@ class App(tk.Tk):
 
         menu_bar.add_cascade(label="File", menu=file_menu)
 
-        # Create the paned window
-        #paned_window = tk.PanedWindow(self, orient=tk.HORIZONTAL, sashwidth=4, sashrelief=tk.RAISED)
-        #paned_window.grid(column=0, row=0)
-
         # Create left frame with tree view
         left_frame = self.create_exp_selector_side()
-        left_frame.grid(column=0, row=0, sticky='NS')
+        left_frame.grid(column=0, row=0, sticky=tk.NS)
 
 
         separator = ttk.Separator(self, orient='vertical')
@@ -117,7 +248,7 @@ class App(tk.Tk):
 
         # Create right frame with tab panel
         right_frame = self.create_plot_side()
-        right_frame.grid(column=1, row=0, sticky=tk.NS)
+        right_frame.grid(column=2, row=0, sticky=tk.NS)
 
 
     def open_file(self):
@@ -128,22 +259,55 @@ class App(tk.Tk):
         # Open file dialog to select folder
         folder_path = filedialog.askdirectory()
 
-        # Add folder to tree view
-        self.tree.insert("", "end", text=folder_path, iid=self.num_tree_items, values=(), open=True)
-        root_folder_iid = self.num_tree_items
-        self.num_tree_items += 1
-        folder_content = os.listdir(folder_path)
-        exp_folders = []
-        for content in folder_content:
-            if osp.isdir(osp.join(folder_path, content)):
-                exp_folders.append(content)
-                self.tree.insert(root_folder_iid, "end", text=content, iid=self.num_tree_items, values=())
-                self.num_tree_items += 1
+        if isinstance(folder_path, str):
+            # Add folder to tree view
+            self.tree.insert("", "end", text=folder_path, iid=self.num_tree_items, values=(), open=True)
+            root_folder_iid = self.num_tree_items
+            self.num_tree_items += 1
+            folder_content = os.listdir(folder_path)
+            exp_folders = []
+            for content in folder_content:
+                content_path = osp.join(folder_path, content)
+                if osp.isdir(content_path):
+                    if 'progress.csv' in os.listdir(content_path):
+                        exp_folders.append(content)
+                        self.tree.insert(root_folder_iid, "end", text=content, iid=self.num_tree_items, values=())
+                        self.num_tree_items += 1
 
     def remove_folder(self):
         # Remove selected item from tree view
-        selected_item = self.tree.selection()[0]
-        self.tree.delete(selected_item)
+        if not len(self.tree.selection()) == 0:
+            selected_item = self.tree.selection()[0]
+            self.tree.delete(selected_item)
+
+    def plot_selected(self):
+        if not len(self.tree.selection()) == 0:
+            is_parent_node = [x in self.tree.get_children() for x in self.tree.selection()]
+            if sum(is_parent_node) == 0:  # root exp folders have no exp to plot
+                tab_name = self.tab_control.select()
+                plot_area = self.tab_control.children[tab_name.split('.')[-1]]
+                progress_files = [osp.join(self.tree.item(self.tree.parent(idx))['text'], self.tree.item(idx)['text'],
+                                           'progress.csv') for idx in self.tree.selection()]
+                plot_area.set_progress_files(progress_files)
+                plot_area.ax.clear()
+                plot_area.plot_update()
+
+    def add2plot(self):
+        if not len(self.tree.selection()) == 0:
+            is_parent_node = [x in self.tree.get_children() for x in self.tree.selection()]
+            if sum(is_parent_node) == 0:  # root exp folders have no exp to plot
+                progress_files = []
+                tab_name = self.tab_control.select()
+                plot_area = self.tab_control.children[tab_name.split('.')[-1]]
+                current_exps = [exp.split('/')[-2] for exp in plot_area.progress_files]
+                for idx in self.tree.selection():
+                    if self.tree.item(idx)['text'] not in current_exps:
+                        plot_area.add_progress_file(osp.join(self.tree.item(self.tree.parent(idx))['text'],
+                                                             self.tree.item(idx)['text'], 'progress.csv'))
+
+
+                plot_area.ax.clear()
+                plot_area.plot_update()
 
 
 if __name__ == "__main__":
