68486e862310baf05fa9a9502081be5dddaf6e0b
luiz <luiz.marao@gmail.com>
2023-07-21 18:00:26 -0300

    create evaluation.py, based on SB3 evaluation.py script, with Rover's agents evaluation criteria. Also, implements an eval_from_exps_root_folder.py file which call the evaluation.py over an experiment folder or in loop for a root folder with several experiments' folders. This later script also reads the exp_call_args.txt files to get the exps' environment' data and recreate them properly.

---
+++ Rover/eval_from_exps_root_folder.py
@@ -0,0 +1,157 @@
+import os
+import sys
+import numpy as np
+from tqdm import tqdm
+import csv
+
+from Rover.utils.logger import safe_print
+from Rover.utils.evaluation import evaluate_policy
+from Rover.algos.ppo.ppo import PPO_Rover
+from Rover.utils.env_register import register_rover_environments
+from Rover.utils.env_util import make_vec_env
+from Rover.utils.arg_parser import common_arg_parser, parse_unknown_args, eval_only_arg_parser
+
+register_rover_environments()
+
+
+def parse_cmdline_kwargs(args):
+    '''
+    convert a list of '='-spaced command-line arguments to a dictionary, evaluating python objects when possible
+    '''
+    def parse(v):
+
+        assert isinstance(v, str)
+        try:
+            return eval(v)
+        except (NameError, SyntaxError):
+            return v
+
+    return {k: parse(v) for k, v in parse_unknown_args(args).items()}
+
+
+def main(args):
+    args_ = args
+    exp_arg_parser = common_arg_parser()
+    eval_arg_parser = eval_only_arg_parser()
+    eval_args, unknown_eval_args = eval_arg_parser.parse_known_args(args)
+    extra_eval_args = parse_cmdline_kwargs(unknown_eval_args)
+    
+    if eval_args.eval_exp_dir is None:
+        return
+    elif not os.path.exists(eval_args.eval_exp_dir):
+        return
+
+    os.environ["MUJOCO_GL"] = 'egl'  # Set mujoco rendering to dedicated GPU
+
+
+    monitor_kwargs = dict(info_keywords=['death', 'goal_reached_flag', 'timeout'], reset_keywords=['current_goal'])
+
+
+    goals = eval_args.eval_goals
+    num_eval_eps = eval_args.num_eval_eps
+    maybe_exp_list = os.listdir(eval_args.eval_exp_dir)
+    eval_dirs = []
+    exps_dirs = []
+    eval_networks = {}
+    num_nets = 0
+    if 'progress.csv' in maybe_exp_list:  # is just one exp folder
+        exp_dir = eval_args.eval_exp_dir
+        eval_dir = os.path.join(eval_args.eval_exp_dir, 'saved_networks')
+        if os.path.exists(eval_dir):
+            eval_dirs.append(eval_dir)
+            exps_dirs.append(exp_dir)
+    else:  # might be a root folder for multiple exps
+        for content in maybe_exp_list:
+            content_path = os.path.join(eval_args.eval_exp_dir, content)
+            if os.path.isdir(content_path):
+                if 'progress.csv' in os.listdir(content_path):  # Is an Exp Folder, search for saved data
+                    exp_dir = content_path
+                    eval_dir = os.path.join(content_path, 'saved_networks')
+                    if os.path.exists(eval_dir):
+                        eval_dirs.append(eval_dir)
+                        exps_dirs.append(exp_dir)
+                        nets = os.listdir(eval_dir)
+                        eval_networks[eval_dir] = nets
+                        num_nets += len(nets)
+    pbar_full = tqdm(desc='Total Evaluation Episodes', total=num_eval_eps * num_nets * len(goals), position=2)
+    for idx, exp_dir in enumerate(exps_dirs):
+        if len(exps_dirs) > 1:
+            exp_name = exp_dir.split('/')[-1].split('-')[0]
+            safe_print((' '*5+"##### BEGINING EXP {} EVALUATION #####"+' '*5).format(exp_name))
+        with open(os.path.join(exp_dir, 'exp_call_args.txt')) as exp_call_file:
+            # second line contains the call line, but start with 'python -m Rover.run' and end with '\n'
+            exp_call_line = exp_call_file.readlines()[1][:-2]
+            exp_args = exp_call_line.split(' ')  # separate args keeping format '--arg'
+        # ENVIRONMENT PARAMETERS
+        exp_args, unknown_exp_args = exp_arg_parser.parse_known_args(exp_args)
+        extra_exp_args = parse_cmdline_kwargs(unknown_exp_args)
+        if isinstance(exp_args.img_red_size, str):
+            exp_args.img_red_size = eval(exp_args.img_red_size)
+        rover_env = exp_args.env
+        env_kwargs = {"render_mode": "rgb_array", 'gamma': exp_args.gamma,
+                          'start_at_initpos': exp_args.start_at_initpos,
+                          'end_after_current_goal': not exp_args.dont_end_after_current_goal,
+                          'random_current_goal': not exp_args.dont_random_current_goal}
+        num_environments = exp_args.num_env
+        env_kwargs.update(extra_exp_args)
+        if rover_env.startswith('Rover4We'):
+            env_kwargs.update(
+                {"width": 440, "height": 270, "save_images": args.save_images, "verbose": args.env_verbose,
+                 "img_reduced_size": args.img_red_size})
+            if 'Encoded' in rover_env:  #
+                assert args.encoder_name is not None, 'This environment MUST receive a proper encoder name to work'
+                env_kwargs.update({'encoder_name': args.encoder_name})
+
+        # RECREATE THE EXP'S ENV
+        envs = make_vec_env(rover_env, env_kwargs=env_kwargs, n_envs=num_environments, monitor_kwargs=monitor_kwargs)
+
+        # RUN EVAL PHASE FOR THE EXP
+        error_loading = []
+        eval_results = {}
+        for goal in goals:
+            eval_results[goal] = []
+        eval_nets_list = os.listdir(eval_dirs[idx])
+        for net in eval_nets_list:
+            net_path = os.path.join(eval_dirs[idx], net)
+            net_num = net.split('.')[0]
+            try:
+                model = PPO_Rover.load(path=net_path)
+            except:
+                error_loading.append(net)
+                safe_print('Network {} skipped due to loading error.'.format(net))
+                continue
+            results = evaluate_policy(model=model, env=envs, net_num=net_num, n_eval_episodes=num_eval_eps,
+                                      progress_bar_full=pbar_full)
+            for goal in goals:
+                eval_results[goal].append(results[goal])
+
+        # TREATS ACQUIRED DATA BEFORE WRITING EXP'S RESULTS
+        for goal in goals:
+            if goal == -1:
+                file_path = os.path.join(exp_dir, 'Eval_complete_mode.csv')
+            else:
+                file_path = os.path.join(exp_dir, 'Eval_goal{}_mode.csv'.format(goal))
+            file = open(file_path, 'w')
+            for net_data in eval_results[goal]:
+                gr_eps = net_data.pop('goal_reached_episodes')
+                if goal == -1:
+                    for g in range(3):
+                        net_data['g{}_reached_eps'.format(g)] = gr_eps[g]
+                        net_data['g{}_reached_eps (%)'.format(g)] = 100 * gr_eps[g] / net_data['total_eps']
+                else:
+                    net_data['g{}_reached_eps'.format(goal)] = gr_eps[goal]
+                    net_data['g{}_reached_eps (%)'.format(goal)] = 100 * gr_eps[goal] / net_data['total_eps']
+
+            writer = csv.DictWriter(file, eval_results[goal][0].keys())
+            writer.writeheader()
+            writer.writerows(eval_results[goal])
+            file.close()
+        if len(error_loading) > 0:
+            safe_print('Network(s) {} skipped due to loading error.'.format(', '.join(error_loading)))
+        envs.close()  # CLOSE ENV FOR NEW RUN IF MORE EXPS TO RUN
+        print()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
---
+++ Rover/utils/evaluation.py
@@ -0,0 +1,141 @@
+from typing import Dict, List, Tuple, Union
+
+import gymnasium as gym
+import numpy as np
+from tqdm import tqdm
+from Rover.utils.logger import safe_print
+
+from stable_baselines3.common import type_aliases
+from stable_baselines3.common.vec_env import VecEnv
+
+
+def evaluate_policy(
+        model: "type_aliases.PolicyPredictor",
+        env: Union[gym.Env, VecEnv],
+        net_num: str = None,
+        n_eval_episodes: int = 10,
+        deterministic: bool = True,
+        render: bool = False,
+        goals: List = [-1, 1, 2],
+        progress_bar_full = None
+) -> Dict:
+    """
+    Runs Rover's policy for ``n_eval_episodes`` episodes and returns tracked data.
+
+    :param net_num: Agent's network number, to be in the returned dict
+    :param progress_bar_full: A progress bar to be updated if there is a loop calling this function
+    :param goals: the listo of goals in which the agent will be evaluated. -1 means complete challenge track
+    :param model: The Rover agent you want to evaluate. This can be any object
+        that implements a `predict` method, such as an RL algorithm (``BaseAlgorithm``)
+        or policy (``BasePolicy``).
+    :param env: The Rover ``VecEnv`` environment.
+    :param n_eval_episodes: Number of episode to evaluate the agent
+    :param deterministic: Whether to use deterministic or stochastic actions
+    :param render: Whether to render the environment or not
+    :return: dict with several evaluated information.
+    """
+
+    if net_num is None:
+        print("This function needs the network name to run properly")
+    n_envs = env.num_envs
+    eval_results = {}
+
+    pbar_net = tqdm(desc='Current Net Episodes', total=n_eval_episodes * len(goals), position=1)
+    pbar_goal = tqdm(desc='Current Net and Goal', total=n_eval_episodes, position=0)
+    pbars_list = [pbar_net, pbar_goal]
+    if progress_bar_full is not None:
+        pbars_list.append(progress_bar_full)
+
+    def pbars_update(n):
+        [bar.update(n) for bar in pbars_list]
+
+    for goal in goals:
+        if goal == -1:
+            env.env_method('set_init_configs_full')
+            safe_print("Evaluating Network {} in complete mode".format(net_num))
+        else:
+            safe_print("Evaluating Network {} in goal_{} mode".format(net_num, goal))
+        if goal == 0:
+            env.env_method('set_init_configs_g0')
+        if goal == 1:
+            env.env_method('set_init_configs_g1')
+        if goal == 2:
+            env.env_method('set_init_configs_g2')
+
+        episode_rewards = []
+        episode_lengths = []
+        current_rewards = np.zeros(n_envs)
+        current_lengths = np.zeros(n_envs, dtype="int")
+        observations = env.reset()
+        episode_counts = np.zeros(n_envs, dtype="int")
+        timeouts = 0
+        deaths = 0
+        goal_reached_episodes = [0, 0, 0]
+
+        # Divides episodes among different sub environments in the vector as evenly as possible
+        episode_count_targets = np.array([(n_eval_episodes + i) // n_envs for i in range(n_envs)], dtype="int")
+        while (episode_counts < episode_count_targets).any():
+            actions, _ = model.predict(observation=observations, deterministic=deterministic)
+            observations, rewards, dones, infos = env.step(actions)
+            current_rewards += rewards
+            current_lengths += 1
+            for i in range(n_envs):
+                if episode_counts[i] < episode_count_targets[i]:
+                    info = infos[i]
+                    if dones[i]:
+                        if "episode" in info.keys():
+                            # Do not trust "done" with episode endings.
+                            # Monitor wrapper includes "episode" key in info if environment
+                            # has been wrapped with it. Use those rewards instead.
+                            episode_rewards.append(info["episode"]["r"])
+                            episode_lengths.append(info["episode"]["l"])
+                            # Only increment at the real end of an episode
+                            episode_counts[i] += 1
+                            pbars_update(1)
+                        else:
+                            episode_rewards.append(current_rewards[i])
+                            episode_lengths.append(current_lengths[i])
+                            episode_counts[i] += 1
+                            pbars_update(1)
+                        current_rewards[i] = 0
+                        current_lengths[i] = 0
+
+                    if "timeout" in info.keys():
+                        timeouts += 1
+                    if "goal_reached_flag" in info.keys():
+                        goal_reached = info['goal_reached_flag']
+                        goal_reached_episodes[int(goal_reached)] += 1
+                    if "death" in info.keys():
+                        deaths += 1
+
+        mean_reward = np.mean(episode_rewards)
+        std_reward = np.std(episode_rewards)
+        min_reward = np.min(episode_rewards)
+        max_reward = np.max(episode_rewards)
+        total_eps = len(episode_rewards)
+        mean_length = np.mean(episode_lengths)
+        std_length = np.std(episode_lengths)
+        min_length = np.min(episode_lengths)
+        max_length = np.max(episode_lengths)
+        eval_results[goal] = {'Network': net_num,
+                              'ep_rew_mean': mean_reward,
+                              'ep_rew_std': std_reward,
+                              'ep_rew_min': min_reward,
+                              'ep_rew_max': max_reward,
+                              'total_eps': total_eps,
+                              'timeout_eps': timeouts,
+                              'deaths': deaths,
+                              'deaths (%)': 100 * deaths / total_eps,
+                              'goal_reached_episodes': goal_reached_episodes,
+                              'ep_len_mean': mean_length,
+                              'ep_len_std': std_length,
+                              'ep_len_min': min_length,
+                              'ep_len_max': max_length,
+                              }
+
+        pbar_goal.reset()
+
+        if render:
+            env.render()
+
+    return eval_results
